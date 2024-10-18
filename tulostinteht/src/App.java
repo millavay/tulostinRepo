e14d881b2704668b1caeb8d9ce0e4c99a029521a
Milla Väyrynen <milla.vayrynen@gmail.com>
2024-10-18 13:18:41 +0300

    java projektin luonti

---
+++ tulostinteht/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String nimi = "Milla";
+        int luku1 = 8;
+        int luku2 = 2;
+        int tulo = luku1 * luku2;
+
+        System.out.println("Hei olen tulostin ohjelma");
+        System.out.println("Ohjelman tekijä: " + nimi);
+        System.out.println("Luku1 muuttujan arvo on: " + luku1);
+        System.out.println("Luku2 muuttujan arvo on: " +luku2);
+        System.out.println(luku1 + " * " + luku2 + " = " + tulo );
+
+
+
+    }
+}
